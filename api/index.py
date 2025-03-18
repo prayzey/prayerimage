0abde9ec7f14e8e9078bb5c4b8f9d800310dbb3b
prayzey <praiseadesokan@gmail.com>
2025-03-17 22:26:52 -0400

    Prepare project for Railway deployment

--- api/index.py
+++ api/index.py
@@ -83,4 +83,5 @@ def home():
     return 'Prayer Image Generator API is running!'
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
